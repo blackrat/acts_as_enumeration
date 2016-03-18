3ba3f2213b43f86852fce30f9af6986462ec8c72
Paul McKibbin <pmckibbin@gmail.com>
2016-03-18 18:37:23 +0000

    Testing with init.rb and acts_as_enumeration.rb for differing rails versions

--- lib/active_record/acts/enumeration.rb
+++ lib/active_record/acts/enumeration.rb
@@ -1,7 +1,7 @@
 module ActiveRecord
   module Acts
     module Enumeration
-      VERSION='0.1.2'
+      VERSION='0.1.3'
       class << self
         def included(base)
           base.class_eval do
